5ea14c4102a401d8d25970de973e13949f33317e
Amita Kashid <49828170+AmitaKashid@users.noreply.github.com>
2023-10-26 10:13:17 +0200

    Update Programming01.java
    Added part b

--- P01/src/com/programming01/Programming01.java
+++ P01/src/com/programming01/Programming01.java
@@ -12,6 +12,11 @@ import java.io.StringReader;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.lucene.analysis.CharArraySet;
+
 public class Programming01 {
 	
 	private static final String TEXT = "Today is sunny. She is a sunny girl. To be or not to be. She is in Berlin today. Sunny Berlin! Berlin is always exciting!";
@@ -20,10 +25,14 @@ public class Programming01 {
 		// Call to the Tokenizer - As per Programming Assignment 1 - sub-question 'a'
 		System.out.println("------------------- Task P01 - a   -------------------");
 		tokenizerTaskA();
+
+		// Call to the Analyser - As per Programming Assignment 1 - sub-question 'c'
+		System.out.println("------------------- Task P01 - b   -------------------");
+		stopWord(); // call stopWord Function
 		
 		// Call to the Analyser - As per Programming Assignment 1 - sub-question 'c'
 		System.out.println("------------------- Task P01 - c   -------------------");
-    	analyser();
+    		analyser();
         
     }
     
@@ -90,6 +99,27 @@ public class Programming01 {
         System.out.println("\nTokens generated using standard tokenizer:");
         standardanalyzer(standardAnalyzer, TEXT);
     }
+
+	private static void stopWord() {
+	try {
+		System.out.println("Stopword eliminator");
+		List<String> stopWords=Arrays.asList("was", "is", "in", "to", "be");
+		CharArraySet stopSet=new CharArraySet(stopWords, true);
+		Analyzer analyzer= new StandardAnalyzer(stopSet);
+		List<String> result = new ArrayList<String>();
+		TokenStream stream  = analyzer.tokenStream("",TEXT);
+		stream.reset();
+		while (stream.incrementToken()) {
+			result.add(stream.addAttribute(CharTermAttribute.class).toString());
+		}
+		System.out.println(result);
+	} catch (IOException e) {
+		// not thrown b/c we're using a string reader...
+		throw new RuntimeException(e);
+	}
+
+	
+}
 
 }
 
